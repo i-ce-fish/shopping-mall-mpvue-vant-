59ca06dc1f788c9ed4372156e22e0c5e9c7f7b19
binyu <binyu>
2020-02-27 00:47:41 +0800

    亲友圈

---
+++ src/pages/my/friend/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+        <div class="">
+          <van-cell-group>
+            <van-cell title="姓名" label="手机号码"   />
+            <van-cell title="姓名"  label="手机号码"  />
+            <van-cell title="姓名" label="手机号码"  />
+            <van-cell title="姓名" label="手机号码"  />
+          </van-cell-group>
+        </div>
+    <van-divider contentPosition="center">到底了</van-divider>
+  </div>
+</template>
+<script>
+  import tuiListCell from "@/components/list-cell/list-cell"
+  import tuiListView from "@/components/list-view/list-view"
+
+  export default {
+    components: {
+      tuiListView,
+      tuiListCell
+    },
+    data() {
+      return {
+        active:0,
+
+
+        currentTab: 0,
+        pageIndex: 1,
+        loadding: false,
+        pullUpOn: true,
+        scrollTop: 0
+      }
+    },
+    methods: {
+      change(e) {
+        this.currentTab = e.index
+      },
+      detail() {
+      }
+    }
+  }
+</script>
+<style lang="scss">
+</style>
+
+
