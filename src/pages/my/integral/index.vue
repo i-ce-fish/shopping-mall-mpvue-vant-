781854440e0e4c5b7e16a0145107c588228356b7
binyu <binyu>
2020-02-27 00:48:13 +0800

    积分

--- src/pages/my/integral/index.vue
+++ src/pages/my/integral/index.vue
@@ -1,128 +1,73 @@
 <template>
   <div class="container">
-    <van-collapse :value="activeNames" @change="onChange">
-      <van-collapse-item title="积分-等级-折率规定" name="1">
-        <div class="line-box">
-          <div class="tui-flex tui-center y-font-m ">
-            <div class="tui-flex-1">
-              会员等级
-            </div>
-            <div class="tui-flex-1">
-              新品折率
-            </div>
-            <div class="tui-flex-1">
-              积分
-            </div>
-          </div>
+    <van-tabs swipeable scrollspy sticky :active="active" @change="onChange">
+      <van-tab title="全部明细">
+        <div class="">
+          <van-cell-group>
+<!--            todo 1. 通过增加类名来改变样式（无法覆盖已有样式） 2. 使用插槽 -->
+            <van-cell title="使用" label="2020-12-12 22:13:12"  value="-100" />
+            <van-cell title="使用"  label="2020-12-12 22:13:12" value="-100" />
+            <van-cell title="使用" label="2020-12-12 22:13:12" value="-100" />
+            <van-cell title="使用" label="2020-12-12 22:13:12" value="-100" />
+          </van-cell-group>
         </div>
-        <div class="line-box">
-          <div class="tui-flex tui-center">
-            <div class="tui-flex-1">
-              一级会员
-            </div>
-            <div class="tui-flex-1">
-              95折
-            </div>
-            <div class="tui-flex-1">
-              小于1000分
-            </div>
-          </div>
-        </div>
-        <div class="line-box">
-          <div class="tui-flex tui-center">
-            <div class="tui-flex-1">
-              二级会员
-            </div>
-            <div class="tui-flex-1">
-              88折
-            </div>
-            <div class="tui-flex-1">
-              1000分-1999分
-            </div>
-          </div>
-        </div>
-        <div class="line-box">
-          <div class="tui-flex tui-center">
-            <div class="tui-flex-1">
-              三级会员
-            </div>
-            <div class="tui-flex-1">
-              85折
-            </div>
-            <div class="tui-flex-1">
-              2000分及以上
-            </div>
-          </div>
-        </div>
+<!--        <view>-->
+<!--          <view class="tui-order-item" v-for="(item,indexA) in 1 "  :key="indexA"  >-->
 
-      </van-collapse-item>
-      <van-collapse-item title="如何获得积分" name="2">
-        <div class="" v-for="(item,index) of integralWay">
-          {{item}}
-        </div>
-      </van-collapse-item>
-      <van-collapse-item title=" 引荐亲友红包" name="3">
-        <div class="" v-for="(item,index) of integralWay">
-          {{item}}
-        </div>
-      </van-collapse-item>
-      <van-collapse-item title="积分兑换规则" name="4">
-        <div class="" v-for="(item,index) of integralWay">
-          {{item}}
-        </div>
-      </van-collapse-item>
-    </van-collapse>
+<!--                <view class="tui-goods-item">-->
+<!--                  <image :src="testImg" class="tui-goods-img"></image>-->
+<!--                  <view class="tui-goods-center">-->
+<!--                    <view class="tui-goods-name ">1000-100券</view>-->
+<!--                    <view class="tui-goods-des y-font-s">仅限男装可用</view>-->
+<!--                    <view class="tui-goods-attr">2020.02.26-2020-12-31</view>-->
+<!--                  </view>-->
+
+<!--                </view>-->
+<!--          </view>-->
+<!--        </view>-->
+      </van-tab>
+      <van-tab title="增加记录">
+        <van-cell title="增加" label="2020-12-12 22:13:12" value="-100" />
+        <van-cell title="增加" label="2020-12-12 22:13:12" value="-100" />
+        <van-cell title="增加" label="2020-12-12 22:13:12" value="-100" />
+        <van-cell title="增加" label="2020-12-12 22:13:12" value="-100" />
+      </van-tab>
+      <van-tab title="使用记录">内容 3</van-tab>
+    </van-tabs>
+    <van-divider contentPosition="center">到底了</van-divider>
   </div>
 </template>
 <script>
+  import tuiListCell from "@/components/list-cell/list-cell"
+  import tuiListView from "@/components/list-view/list-view"
 
   export default {
-    components: {},
-    onLoad() {
-    },
-    onShow() {
-      this.init()
-    },
-    methods: {
-      onChange(event) {
-        console.log(event)
-        this.activeNames = event.mp.detail
-      },
-
-      /**
-       * 通过递归，【同步】执行自动打开折叠面板
-       */
-      init() {
-        //结束递归的条件
-        if (this.activeNames.length < this.collapseSize) {
-          setTimeout(() => {
-            //业务代码
-            this.activeNames.push((this.activeNames.length + 1).toString())
-            //递归
-            this.init()
-          }, 600)
-        }
-      }
-
+    components: {
+      tuiListView,
+      tuiListCell
     },
     data() {
       return {
-        collapseSize: 4,
-        activeNames: [],
-        integralWay: [
-          "1、购物1元，赠送1分。",
-          "2、促销时，购物1元，可能获得双倍积分。",
-          "3、给店铺帮忙（介绍顾客、提建议等等），也能获得积分赠送。",
-          "4、本店积分能兑换现金。",
-          "5、详情请关注店铺公告。"
-        ]
+        active:0,
+
+
+        currentTab: 0,
+        pageIndex: 1,
+        loadding: false,
+        pullUpOn: true,
+        scrollTop: 0
+      }
+    },
+    methods: {
+      change(e) {
+        this.currentTab = e.index
+      },
+      detail() {
       }
     }
   }
 </script>
 <style lang="scss">
-  .container {
-  }
 </style>
 
 
