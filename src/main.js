addc1171910ee828d53ea340b65dd063ce7b0dcf
binyu <binyu>
2020-02-27 00:50:24 +0800

    app.json

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App'
-import {request} from "@/utils/request";
 import {FORWARD_URL} from "@/utils/api";
 
 Vue.config.productionTip = false;
@@ -36,18 +35,19 @@ Vue.mixin({
 // 所有页面都转发
 // ref：https://blog.csdn.net/lhy349/article/details/81188332
 Vue.mixin({
-  onShareAppMessage() {
-    request(
-      FORWARD_URL,
-      'GET',
-      {}
-    );
-    return {
-      title: 'test',
-      path: 'pages/home/index/main',
-      imageUrl: '/static/img/xuanchuan.png'
-    }
-  }
+  //分享页
+  // onShareAppMessage() {
+  //   request(
+  //     FORWARD_URL,
+  //     'GET',
+  //     {}
+  //   );
+  //   return {
+  //     title: 'test',
+  //     path: 'pages/home/index/main',
+  //     imageUrl: '/static/img/xuanchuan.png'
+  //   }
+  // }
 });
 
 const app = new Vue(App)
