bf994413fe5b9843b325602052b52c701270c660
binyu <binyu>
2020-03-03 02:32:28 +0800

    单选、多选、日期选择、城市选择、步进器、开关组件

--- src/pages/my/info/index.vue
+++ src/pages/my/info/index.vue
@@ -3,25 +3,70 @@
     <form report-submit="true">
       <tui-list-cell :hover="false" padding="0">
         <view class="tui-line-cell">
-          <view class="tui-title">身高</view>
+          <view class="tui-title">文本输入框</view>
           <input placeholder-class="tui-phcolor" class="tui-input" name="name" placeholder="请输入收货人姓名" maxlength="15"
                  type="text"/>
         </view>
       </tui-list-cell>
       <tui-list-cell :hover="false" padding="0">
+        <view class="tui-line-cell tui-flex">
+          <view class="tui-title">单选</view>
+          <van-radio-group class="tui-flex-1 tui-flex tui-align-between " :value="radio" @change="onChange">
+            <van-radio name="1">单选框 1</van-radio>
+            <van-radio name="2">单选框 2</van-radio>
+          </van-radio-group>
+        </view>
+      </tui-list-cell>
+      <tui-list-cell :hover="false" padding="0">
+        <view class="tui-line-cell tui-flex">
+          <view class="tui-title">多选</view>
+          <van-checkbox-group class="tui-flex-1 tui-flex tui-align-between"
+                              :value="checkbox"
+                              @change="onCheckbox">
+            <van-checkbox shape="square" name="a">a</van-checkbox>
+            <van-checkbox  shape="square" name="b">b</van-checkbox>
+            <van-checkbox  shape="square"name="c">c</van-checkbox>
+          </van-checkbox-group>
+        </view>
+      </tui-list-cell>
+      <tui-list-cell :hover="false" padding="0">
         <view class="tui-line-cell">
-          <view class="tui-title">体重</view>
+          <view class="tui-title">日期选择</view>
+          <div @tap="showDatetime=true" placeholder-class="tui-phcolor" class="tui-input">
+            {{dateTime}}
+          </div>
+        </view>
+      </tui-list-cell>
+      <tui-list-cell :hover="false" padding="0">
+        <view class="tui-line-cell">
+          <view class="tui-title" @tap="showArea = true">城市选择</view>
+          <view placeholder-class="tui-phcolor" class="tui-input"
+                 @tap="showArea = true">
+            <view>
+              {{areaValue}}
+            </view>
+          </view>
+        </view>
+      </tui-list-cell>
+      <tui-list-cell :hover="false" padding="0">
+        <view class="tui-line-cell">
+          <view class="tui-title">数字输入</view>
           <input placeholder-class="tui-phcolor" class="tui-input" name="mobile" placeholder="请输入收货人手机号码" maxlength="11"
                  type="text"/>
         </view>
       </tui-list-cell>
-      <tui-list-cell :arrow="true" padding="0">
+      <tui-list-cell :hover="false" padding="0">
+        <view class="tui-line-cell">
+          <view class="tui-title">步进器</view>
+          <van-stepper :value="stepper" @change="onChangeStepper" />
+        </view>
+      </tui-list-cell>
+      <tui-list-cell :arrow="false" padding="0">
         <view class="tui-line-cell">
           <view class="tui-title">
-            <text class="tui-title-city-text">所在城市</text>
+            <text class="tui-title-city-text">单列选择</text>
           </view>
-          <input placeholder-class="tui-phcolor" class="tui-input" disabled name="city" placeholder="请选择城市"
-                 maxlength="50" type="text"/>
+         <view class="tui-input" @tap="showPicker = true" >{{pickerValue}}</view>
         </view>
       </tui-list-cell>
 <!--      <tui-list-cell :hover="false" padding="0">-->
@@ -44,7 +89,7 @@
       <!-- 默认地址 -->
       <tui-list-cell :hover="false" padding="0">
         <view class="tui-swipe-cell">
-          <view>设为默认</view>
+          <view>开关</view>
           <switch checked color="#30CC67" class="tui-switch-small"/>
         </view>
       </tui-list-cell>
@@ -54,26 +99,113 @@
       </view>
 
     </form>
+
+<!--    弹出日期选择-->
+    <van-popup :show="showDatetime" position="bottom" @close="showDatetime = false">
+<!--      todo  3 errors  formatter not a function -->
+      <van-datetime-picker
+        type="date"
+        :value="currentDate "
+        @input="onDate"
+        :min-date="minDate"
+        :formatter="formatter"
+        @cancel="showDatetime = false"
+        @confirm="onConfirmDatetime"
+      />
+    </van-popup>
+<!--    弹出城市选择-->
+
+    <van-popup :show="showArea" position="bottom" @close="showArea= false">
+    <van-area
+      @confirm="onConfirmArea"
+      @cancel="showArea= false"
+      :area-list="provinceList"
+      :value="areaValue"
+    />
+    </van-popup>
+
+<!--弹出单列选择-->
+    <van-popup :show="showPicker" position="bottom" @close="showPicker= false">
+      <van-picker :columns="pickerList" @change="onChangePicker" />
+    </van-popup>
+
   </view>
 </template>
 <script>
   import tuiListCell from "@/components/list-cell/list-cell"
   import tuiListView from "@/components/list-view/list-view"
-
+  import provinceList from '@/utils/area'
   export default {
     components: {
       tuiListCell, tuiListView
     },
-    onLoad() {
+    created(){
+
     },
     data() {
       return {
         testImg: "https://source.unsplash.com/random",
-        lists: ["公司", "家", "学校", "其他"]
+        lists: ["公司", "家", "学校", "其他"],
 
+          radio: '1',
+        checkbox: ['a'],
+        currentDate: new Date().getTime(),
+        minDate: new Date().getTime(),
+        dateTime: "请选择日期",
+        showDatetime: false,
+        formatter(type, value) {
+              if (type === 'year') {
+                return `${value}年`;
+              } else if (type === 'month') {
+                return `${value}月`;
+              }
+              return value;
+          },
+
+        showArea: false,
+        provinceList: provinceList,
+        areaValue: "请选择城市",
+        stepper: 1,
+        showPicker: false,
+        pickerValue: "请选择",
+        pickerList: [1,2,2,3,5,6,5]
       }
     },
-    methods: {}
+
+    methods: {
+      onChange(e) {
+        console.log(e)
+        if (e.mp.detail === this.radio) {
+          this.radio = null
+        }else {
+        this.radio = e.mp.detail;
+
+        }
+      },
+      onCheckbox(e) {
+        this.checkbox = e.mp.detail
+      },
+      onDate(e) {
+        this.currentDate = e.mp.detail
+      },
+      onConfirmDatetime(e) {
+        this.dateTime = e.mp.detail
+        this.showDatetime = false
+      },
+      onConfirmArea(e) {
+        console.log(e.mp.detail)
+        this.areaValue = e.mp.detail.values[2].name
+        this.showArea = false
+      },
+      onChangeStepper(e) {
+        this.stepper = e.mp.detail
+
+      },
+      onChangePicker(e) {
+        console.log(e)
+        this.pickerValue = e.mp.detail.value
+      }
+    },
   }
 </script>
 <style lang="scss">
